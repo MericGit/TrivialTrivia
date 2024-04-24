437bfe9f526cae07994842dc1ec2bdbf8322536b
MericGit <41242144+MericGit@users.noreply.github.com>
2024-04-24 15:59:36 -0400

    init

---
+++ p4/flask_app/config.py
@@ -0,0 +1,3 @@
+# Stores all configuration values
+SECRET_KEY = b'\x88\xeb\x83\xb9\xac\x8dv\xa2^%T+\x01\x91A\x1a'
+MONGODB_HOST = "mongodb+srv://lzhang34:XAKq9aOu@388j.xxjfouq.mongodb.net/388j?retryWrites=true&w=majority"
