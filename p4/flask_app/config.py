adf490186f8a3260d211eb96a1b61f2b250a924f
Dhruv Rao <rao_dhruv@yahoo.com>
2024-04-25 00:06:43 -0400

    hello

--- p4/flask_app/config.py
+++ p4/flask_app/config.py
@@ -1,10 +1,6 @@
 # Stores all configuration values
 SECRET_KEY = b'\x88\xeb\x83\xb9\xac\x8dv\xa2^%T+\x01\x91A\x1a'
-MONGODB_HOST = "mongodb+srv://lzhang34:XAKq9aOu@388j.xxjfouq.mongodb.net/388j?retryWrites=true&w=majority"
-
-
-
-#mongodb+srv://lzhang34:ITnqjENW1qdoCUul@trivialtrivia.qyxgxjd.mongodb.net/?retryWrites=true&w=majority&appName=TrivialTrivia
+MONGODB_HOST = "mongodb+srv://lzhang34:ITnqjENW1qdoCUul@trivialtrivia.qyxgxjd.mongodb.net/?retryWrites=true&w=majority&appName=TrivialTrivia"
 
 """
 MongoDB:
